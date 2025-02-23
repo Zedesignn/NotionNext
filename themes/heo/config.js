e200a9a6860278a1cab5cba6efdd7679715bdc30
Zephyr Builds <160080851+Zedesignn@users.noreply.github.com>
2025-02-24 05:58:27 +1100

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://www.xiaohongshu.com/user/profile/5fcda723000000000101eb94?xsec_token=YBsZ0FNu-Z8jC0f0qcy0DCCCd7kJQJ7lxDblKxevWhLrQ=&xsec_source=app_share&xhsshare=CopyLink&appuid=5fcda723000000000101eb94&apptime=1740305082&share_id=548e10fb14d84affb8be69658db50d93',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
