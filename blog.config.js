1bd55928c8ecc1529e54a0f206bbbec48b5a3afa
Zephyr Lin <hi@zbuilds.com.au>
2025-10-10 10:00:36 +1100

    chore: update api base url config

--- blog.config.js
+++ blog.config.js
@@ -5,6 +5,9 @@ const BLOG = {
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
     '02ab3b8678004aa69e9e415905ef32a5,en:7c1d570661754c8fbc568e00a01fd70e',
+  API_BASE_URL:
+    process.env.API_BASE_URL ||
+    'https://zephyrlin.notion.site/api/v3', // 当官方API异常时可替换为站点分配的域名
   THEME: process.env.NEXT_PUBLIC_THEME || 'heo', // 将主题改为 heo
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'auto', // HEO主题建议使用auto
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6],
@@ -27,10 +30,10 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Zephyr', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '独立设计师，建造师，造价师', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'zbuilds.com.au', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Zephyr画话澳洲工程', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
